2b03eb18195a2efdac543e96dc7ec2664dab67b2
Shashank <s_shashank@outlook.com>
2020-11-16 23:14:27 +0530

    initial commit

---
+++ server/app.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const { graphqlHTTP } = require("express-graphql");
+const schema = require("./schema/schema");
+
+const app = express();
+
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema: schema,
+    graphiql: true,
+  })
+);
+
+const port = 4000;
+
+app.listen(port, () => {
+  console.log(`Server is running at port ${port}`);
+});
